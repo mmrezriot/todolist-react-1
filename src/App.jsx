4689daec1e8b7b34429c2e3589fb6fb98b98ad5e
mmrez <mohammdreza.riot7676@gmail.com>
2025-08-03 18:36:43 +0330

    Implement context for task management and update App component to use itemContex

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import HeaderList from './headerList.jsx'
 import ItemList from './ItemList.jsx'
+import { itemContex } from './context.jsx'
 
 function App() {
+  
+  
+  const [taskItem , setTaskItem] = useState([
+    {
+      title:"آیتم تستی اول",
+      id : 1,
+      done:true
+    },
+    {
+      title:"آیتم تستی دوم",
+      id : 2,
+      done:false
+    }
+])
   
 
   return (
     <>
+    <itemContex.Provider value={
+     { taskItem,
+      setTaskItem}
+    }>
 <div className="container py-4">
   <HeaderList/>
 <div className="item-list mb-5 text-center">
 <ItemList/> 
   </div>
 </div>
+</itemContex.Provider>
 
     </>
   )
