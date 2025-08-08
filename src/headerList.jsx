f47e6fe7d754455f9bf950481f01d9c1ea027e12
mmrez <mohammdreza.riot7676@gmail.com>
2025-08-08 16:05:19 +0330

    Refactor HeaderList component to integrate task management context and update task input handling

--- src/headerList.jsx
+++ src/headerList.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { itemContex } from './context';
 import './App.css';
 const HeaderList= ()=>{
+
+  const {taskItem , setTaskItem} = useContext(itemContex)
+
+  const [task , setTask]= useState([""])
+
+  const handleSetTask = (e)=>{
+    setTask(e.target.value)
+  } 
+
+  const addTask = (e)=>{
+    e.preventDefault();
+    setTaskItem([...taskItem , {id: crypto.randomUUID() , title : task , done :false}])
+    setTask("")
+  }
+
 
 return(
 <>
 <div className="mb-5 text-center">
-    <h2 className="fw-bold mb-3 text-primary">📝 لیست کارها</h2>
-    <div className="input-group shadow-sm w-75 mx-auto">
-      <input
-        type="text"
-        className="form-control rounded-end"
-        placeholder="چی باید انجام بشه؟"
-      />
-      <button className="btn btn-primary rounded-start" type="button">
-       ایجاد 
-      </button>
-    </div>
-  </div>
+  <h2 className="fw-bold mb-3 text-primary">📝 لیست کارها</h2>
+  <form
+    className="input-group shadow-sm w-75 mx-auto" onSubmit={addTask} >
+    <input
+      type="text"
+      className="form-control rounded-end"
+      placeholder="چی باید انجام بشه؟"
+      onChange={handleSetTask}
+      value={task}
+    />
+    <button className="btn btn-primary rounded-start" type="submit">
+      ایجاد
+    </button>
+  </form>
+</div>
+
 </>
 )
 
